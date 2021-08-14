e80ee662734bd71f6a05ddcb10ec80d0e6f061e8
Alexander Chi <arturochi2@hotmail.com>
2021-08-13 19:30:12 -0500

    Se agregó ajustes para proyectos Se agregó imagen para contacto.png Se agregó css para contacto y proyecto

--- src/views/componets/Contact.js
+++ src/views/componets/Contact.js
@@ -7,27 +7,14 @@ import React, { useState } from 'react';
 
 export function Contact (props) {  
     return (
-        <div className="body__routes">
-            <div className="container__routes"> 
-                <div className="box__routes">
-                    <div className="content__routes" style={{textAlign: 'center'}}> 
-                        <img className="img__not__found" src="./img/nyan-cat.gif" ></img>
-                        <h2 className="wavy"> 
-                            <span style={{'--i': 1}}>C</span>
-                            <span style={{'--i': 2}}>O</span>
-                            <span style={{'--i': 3}}>N</span>
-                            <span style={{'--i': 4}}>S</span>
-                            <span style={{'--i': 5}}>T</span>
-                            <span style={{'--i': 6}}>R</span>
-                            <span style={{'--i': 7}}>U</span>
-                            <span style={{'--i': 8}}>C</span>
-                            <span style={{'--i': 9}}>Í</span>
-                            <span style={{'--i': 10}}>O</span>
-                            <span style={{'--i': 11}}>N</span>
-                            <span style={{'--i': 12}}>.</span>
-                            <span style={{'--i': 13}}>.</span>
-                            <span style={{'--i': 14}}>.</span>
-                        </h2>
+        <div className="body__contact">
+            <div className="container__contact"> 
+                <div className="box__contact">
+                    <div className="content__contact"> 
+                        <img className="img__contact" src="./img/contacto.png" ></img>
+                        <div className="p__contact">
+                            <a>Abrir CV</a>
+                        </div>
                     </div>
                 </div> 
             </div>
